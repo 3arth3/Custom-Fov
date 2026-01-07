d32e965976b1d2b4a01e9666f710db0857cca5ce
3 <168254582+3arth3@users.noreply.github.com>
2026-01-07 22:37:43 +0700

    Update MixinGameOptions.java

--- src/main/java/me/aidrob/mixin/MixinGameOptions.java
+++ src/main/java/me/aidrob/mixin/MixinGameOptions.java
@@ -5,18 +5,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(GameOptions.class)
+@Mixin(value = GameOptions.class, priority = 2000)
 public class MixinGameOptions {
 
-    // Sửa giá trị nhỏ nhất của FOV (mặc định là 30) thành 0
-    @ModifyConstant(method = "<init>", constant = @Constant(intValue = 30))
-    private int modifyMinFov(int constant) {
-        return 0;
+    @ModifyConstant(method = "<init>", constant = @Constant(intValue = 30), expect = 0)
+    private int customMin(int constant) {
+        return 0; // FOV thấp nhất
     }
 
-    // Sửa giá trị lớn nhất của FOV (mặc định là 110) thành 200
-    @ModifyConstant(method = "<init>", constant = @Constant(intValue = 110))
-    private int modifyMaxFov(int constant) {
-        return 200;
+    @ModifyConstant(method = "<init>", constant = @Constant(intValue = 110), expect = 0)
+    private int customMax(int constant) {
+        return 200; // FOV cao nhất
     }
 }
